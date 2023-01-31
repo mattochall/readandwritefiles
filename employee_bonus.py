fe1feeee5434b0461f80b4966956c370ce1cd6a7
Matt Ochall <matt_ochall1@baylor.edu>
2023-01-30 23:21:59 -0600

    commit

--- employee_bonus.py
+++ employee_bonus.py
@@ -2,14 +2,21 @@ import csv
 
 infile = open("EmployeePay.csv", "r")
 csvreader = csv.reader(infile, delimiter=",")
+next(csvreader)
 
-for record in csvreader:
+for item in csvreader:
+
+    total = float(item[3]) + (float(item[3]) * float(item[4]))
+
     print()
-    print("Employee ID: ", record[0])
-    print("First Name:  ", record[1])
-    print("Last Name:   ", record[2])
-    print("Base Salary: ", record[3])
-    print("Bonus:       ", record[4])
+    print("Employee ID: ", item[0])
+    print("First Name:  ", item[1])
+    print("Last Name:   ", item[2])
+    print("Base Salary: ", item[3])
+    print("Bonus:       ", item[4])
+    print("Total:       ", total)
     print()
+
+    input()
 
 infile.close()
