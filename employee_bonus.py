9238f05bf58df2d122bff1f555737fc336a57ff8
Matt Ochall <matt_ochall1@baylor.edu>
2023-01-30 19:28:31 -0600

    commit

--- employee_bonus.py
+++ employee_bonus.py
@@ -6,10 +6,10 @@ csvreader = csv.reader(infile, delimiter=",")
 for record in csvreader:
     print()
     print("Employee ID: ", record[0])
-    print("First Name: ", record[1])
-    print("Last Name: ", record[2])
+    print("First Name:  ", record[1])
+    print("Last Name:   ", record[2])
     print("Base Salary: ", record[3])
-    print("Bonus: ", record[4])
+    print("Bonus:       ", record[4])
     print()
 
 infile.close()
